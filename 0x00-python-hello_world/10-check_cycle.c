eff182d189835d0fd0520a9a87203ed9c3f839f1
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-05-08 18:56:30 -0700

    Add task 10 project 0x00

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if the geven linked list has a cycle or not
+ * @list: the head node of the linked list to check
+ * Return: 0 if no cycle found, 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+
+	listint_t *slow_ptr = NULL, *fast_ptr = NULL;
+
+	if (list == NULL)
+		return (0);
+	slow_ptr = list;
+	fast_ptr = list;
+	while (fast_ptr != NULL)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next;
+		if (fast_ptr == NULL)
+			return (0);
+		if (fast_ptr == list || fast_ptr->next == list)
+			return (1);
+		fast_ptr = fast_ptr->next;
+	}
+	return (0);
+}
