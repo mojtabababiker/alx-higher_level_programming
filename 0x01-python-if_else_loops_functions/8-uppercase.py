1a394dbf49898ed0363200e7156f4adfb13a0ea4
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-05-09 18:21:35 -0700

    Edit task 8, project 0x01, edit code to meet requirment, using two print function with string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+
     for i in str:
         if (i >= 'a' and i <= 'z'):
             i = ord(i) - 32
         print("%c" %i, end='')
-    print("")
+    print("%c" %10, end='')
