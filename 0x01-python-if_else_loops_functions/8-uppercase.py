8441ff79d55a2320db871c035c6f430d21716489
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-05-09 18:09:56 -0700

    Edit task 8, project 0x01, enhance code style

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for i in str:
-        if i >= 'a' and i <= 'z':
+        if (i >= 'a' and i <= 'z'):
             i = chr(ord(i) - 32)
         print("%c" %i, end='')
     print("")
