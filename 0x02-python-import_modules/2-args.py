f2bdf327708d8d510a97c93890c61be11e6899db
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-05-11 14:57:27 -0700

    Edit task 2 project 0x02, typo in condition 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if argc == 1:
         print("0 arguments.")
     elif argc == 2:
-        print("1 arguments:")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(argc - 1))
