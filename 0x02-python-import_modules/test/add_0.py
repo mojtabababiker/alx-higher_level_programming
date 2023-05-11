300b3635a3933a30d07827aa002bdc385445bbd0
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-05-11 08:32:10 -0700

    Add test files directory, project 0x02

---
+++ 0x02-python-import_modules/test/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    '''function that adds to numbers
+
+    Args:
+        a: the first number
+        b: the second number
+    Returns:
+        addition result
+    '''
+    return (a + b)
