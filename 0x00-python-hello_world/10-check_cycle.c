7bed9e9088e56100426f68119fd11ed10586d269
mojtabababiker <mojmohammad98@gmail.com>
2023-08-07 14:03:46 -0700

    Edit task 10, project 0x0 Add extra conditions to fix some critical edges

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 
 	listint_t *slow_ptr = NULL, *fast_ptr = NULL;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 	slow_ptr = list;
 	fast_ptr = list;
@@ -18,9 +18,9 @@ int check_cycle(listint_t *list)
 	{
 		slow_ptr = slow_ptr->next;
 		fast_ptr = fast_ptr->next;
-		if (fast_ptr == NULL)
+		if (fast_ptr == NULL || fast_ptr->next == NULL)
 			return (0);
-		if (fast_ptr == list || fast_ptr->next == list)
+		if (fast_ptr == list || fast_ptr->next == list || fast_ptr->next == slow_ptr)
 			return (1);
 		fast_ptr = fast_ptr->next;
 	}
