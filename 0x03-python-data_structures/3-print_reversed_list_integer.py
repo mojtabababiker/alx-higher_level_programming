d02a5a0c2050ae9c601eb251f8e1cbae73b43509
mojtabababiker <mojmohammad98@gmail.com>
2023-08-13 01:06:00 -0700

    Edit task 3 project 0x03, add a new condition to print newline if the list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -10,6 +10,7 @@ def print_reversed_list_integer(my_list=[]):
     i = len(my_list) - 1
     if i < 0:
         print()
-    while i >= 0:
-        print("{:d}".format(my_list[i]))
-        i -= 1
+    else:
+        while i >= 0:
+            print("{:d}".format(my_list[i]))
+            i -= 1
