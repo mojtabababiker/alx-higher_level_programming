51f1955d35aaf6788c78dc53e4ba878cb59be34b
mojtabababiker <mojmohammad98@gmail.com>
2023-08-18 08:31:02 -0700

    Edit task 102 project 0x04, let the change be at the same dict

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -10,8 +10,11 @@ def complex_delete(a_dictionary: dict, value: any) -> dict:
     Retrun:
         A new dictionary without all the keys with the value value
     '''
-    new_dict = dict()
+    keys = list()
     for key, val in a_dictionary.items():
-        if val != value:
-            new_dict.setdefault(key, val)
-    return (new_dict)
+        if val == value:
+            keys.append(key)
+
+    for key in keys:
+        a_dictionary.pop(key, None)
+    return (a_dictionary)
