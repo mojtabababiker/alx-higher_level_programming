905dd1907cb06923d0d9f7e9810307fd0d3705a5
mojtabababiker <mojmohammed98@gmail.com>
2023-08-29 02:36:48 +0000

    Edit task 4 project 0x05

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,22 +17,3 @@ def list_division(my_list_1, my_list_2, list_length):
         for i in range(list_length - len(new_list)):
             new_list.append(0)
     return new_list
-
-
-if __name__ == "__main__":
-    my_l_1 = [10, 8, 4]
-    my_l_2 = [2, 4, 4]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
-
-    print("--")
-
-    my_l_1 = [10, 8, 4, 4]
-    my_l_2 = [2, 0, "H", 2, 7]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
-
-    my_l_1 = ['h']
-    my_l_2 = [1, 2, 3, 4, 6, 7, 8]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
