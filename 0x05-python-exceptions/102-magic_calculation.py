fd0c08a00dfa434e3cd97f456fc0a12c7d875643
mojtabababiker <mojmohammad98@gmail.com>
2023-08-28 18:56:22 -0700

    Add tasks 100->102 project 0x05

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    while i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+
+            result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return result
+
+
+import dis
+print(dis.dis(magic_calculation))
