6cb438ce3ea2a78b01974c632bdf1d980d2e8004
mojtabababiker <mojmohammed98@gmail.com>
2023-08-29 02:16:54 +0000

    Edit tasks 100->101 remove extra spaces from the print functions, project 0x05

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except Exception as ex:
-        print("Exception: ", ex, file=stderr)
+        print("Exception:", ex, file=stderr)
         return False
+    return True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as err:
-        print("Exception: ", err, file=stderr)
+        print("Exception:", err, file=stderr)
         return None
