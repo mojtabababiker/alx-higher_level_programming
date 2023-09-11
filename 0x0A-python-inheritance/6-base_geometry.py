062f215d295ad928ddb9178bfee7c5e00a81ce42
mojtabababiker <mojmohammad98@gmail.com>
2023-09-11 09:37:39 -0700

    Add task 6, project 0x0A

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,26 @@
+#!/bin/usr/python3
+"""
+Contains the improved version of the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Syntax:
+        BaseGeometry()
+
+    Descriptions:
+        An improved version of the BaseGeometry with the method
+        area(self) definded
+    """
+
+    def area(self):
+        """
+        Syntax:
+            BaseGeometry.area(self)
+
+        Descriptions:
+            Calculate the area of the self and return its value
+        """
+
+        raise Exception("area() is not implemented")
