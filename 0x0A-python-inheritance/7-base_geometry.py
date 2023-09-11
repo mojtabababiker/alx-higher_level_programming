a05eb26f4cf3a3268cd37e2b13ebb369ebf17ff5
mojtabababiker <mojmohammad98@gmail.com>
2023-09-11 10:08:15 -0700

    Add task 7, project 0x0A

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Contains the improved version of the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Syntax:
+        BaseGeometry()
+
+    Descriptions:
+        An improved version of the BaseGeometry with the method
+        area(self) definded
+    """
+
+    def area(self):
+        """
+        Syntax:
+            BaseGeometry.area(self)
+
+        Descriptions:
+            Calculate the area of the self and return its value
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Syntax:
+            BaseGeometry.integer_validator(self, name, value)
+
+        Description:
+            Validate the value variable if its an ineger and greater than 0
+            or not
+        """
+
+        if type(value) is not in [int]:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
