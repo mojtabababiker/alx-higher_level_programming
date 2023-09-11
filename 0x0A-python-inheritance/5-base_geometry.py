e36a6af05dcf1067f6371a5aac5d4e3123c95038
mojtabababiker <mojmohammad98@gmail.com>
2023-09-11 09:28:05 -0700

    Add task 5, project 0x0A

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Syntax:
+        BaseGeometry()
+
+    Description:
+        Is an empty class
+
+    """
+
+    pass
