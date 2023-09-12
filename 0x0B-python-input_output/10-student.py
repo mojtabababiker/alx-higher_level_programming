33515984a28285ea9a4e307d661624c535cbded6
mojtabababiker <mojmohammad98@gmail.com>
2023-09-12 11:01:52 -0700

    Add task 10, project 0x0B

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Contains the class Student(first_name,last_name, age) which
+represents a student informations
+"""
+
+
+class Student:
+    """
+    Syntax:
+        Student(first_name, last_name, age)
+
+    Description:
+        A basic class that represetns a initial informations about
+        students, such aas student name and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        conistructor method, intialize the the Student class with
+        first_name, last_name, and age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return a json representation of the Student instance
+        """
+
+        jdict = dict()
+        if type(attrs) in [list]:
+            for key, val in self.__dict__.items():
+                if key in attrs:
+                    jdicts[key] = val
+            return jdict
+        return self.__dict__
