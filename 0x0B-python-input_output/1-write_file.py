46e2e96a85155502e2c985846c3375adad89d136
mojtabababiker <mojmohammad98@gmail.com>
2023-09-12 07:57:28 -0700

    Add task 1, project 0x0B

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Contains the write_file(filename="", text="") function
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Syntax:
+        write_file(filename="", text="")
+
+    Description:
+        Write the `text` string to the the text file `filename`
+        using the `utf-8` encoding
+    """
+
+    with open(filename, "w", encoding="utf-8") as fh:
+        fh.write(text)
