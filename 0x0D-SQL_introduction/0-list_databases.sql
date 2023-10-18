23a2b656217f1eb694de53b15bb5ef5564b2d9e5
mojtabababiker <mojmohammad98@gmail.com>
2023-10-18 04:04:16 +0300

    Add task 0 and 1, project 0x0c

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- script to list all the databases that are currently on
+-- the running MySQL
+SHOW databases ;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- script that create a new database named hbtn_0c_0
+-- if the Database already exists no error will set
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
