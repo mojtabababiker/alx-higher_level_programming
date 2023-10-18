422b96bdb62ec99a97177d3eacb4894d23c0c617
mojtabababiker <mojmohammad98@gmail.com>
2023-10-18 04:24:43 +0300

    Add task 2, 3 and 4, project 0x0c

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- script that delete a database from the current mysql-server databases
+-- if the database is not in the server no error will be raised
+DROP database IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- script that list the tables of a database on the current mysql server
+-- the tables are queried from the database that enterd as an argument
+SHOW TABLES;
