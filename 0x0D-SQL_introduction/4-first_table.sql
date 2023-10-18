859e94368b50a7fca00babd319986110a8efe910
mojtabababiker <mojmohammad98@gmail.com>
2023-10-18 04:37:37 +0300

    Add task 4, project 0x0c

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- script that creates a table called first_table in the current database
+-- the first_table table has id and name
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
