812728bde20fa5ea5f81849ca92436a9cf11cdb1
mojtabababiker <mojmohammad98@gmail.com>
2023-11-13 20:55:26 +0200

    Edit task 8, project 0x12, change conditions

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -9,7 +9,7 @@ if (isNaN(size)) {
 } else {
     for (let i = 0; i < size; i++) {
 	for (let j = 0; j < size; j++) {
-	    square += 'x';
+	    square += 'X;
 	}
 	console.log(square);
 	square = '';
