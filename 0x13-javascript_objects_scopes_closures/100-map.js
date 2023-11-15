fe4d74e8920febc8010cd2ef3e7769b09bf389c4
mojtabababiker <mojmohammad98@gmail.com>
2023-11-15 05:05:28 +0200

    Add task 100, project 0x12

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 
 const list = require('./100-data.js').list;
 
-const list2 = list.map((x) => x * list.indexOf(x));
+const list2 = list.map((element, index) => element * index);
 console.log(list);
 console.log(list2);
