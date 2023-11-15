8beaa4b50ed47894ed47e20a8705cd4dcba9725b
mojtabababiker <mojmohammad98@gmail.com>
2023-11-15 04:51:43 +0200

    Edit task 5, removes the validations on the consitructor methos, project 0x12

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,9 +4,7 @@ const Rectangle = require('./4-rectangle');
 
 class Sqaure extends Rectangle {
   constructor (size) {
-    if (size > 0) {
-      super(size, size);
-    }
+    super(size, size);
   }
 }
 module.exports = Sqaure;
