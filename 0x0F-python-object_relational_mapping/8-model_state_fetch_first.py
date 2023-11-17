6edbf1d54e0729406b4254ab216f58118cf07b73
mojtabababiker <mojmohammad98@gmail.com>
2023-11-17 22:09:33 +0200

    ADD task 8, project 0x0f

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+model the fitch and print the first state object from the database
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+
+if __name__ == "__main__":
+
+    url = URL.create(
+        drivername='mysql',
+        username=sys.argv[1],
+        password=sys.argv[2],
+        host='localhost',
+        port=3306,
+        database=sys.argv[3]
+        )
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    first_state = session.query(State).first()
+
+    print(f"{first_state.id}: {first_state.name}")
