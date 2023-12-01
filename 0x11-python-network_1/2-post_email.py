01a32819cc98487f938a7dfbfde6ed4e59c74173
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 18:20:52 +0200

    Edit code style and imports method

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ send an email using urllib.request module """
 import sys
-import urllib.request, urllib.parse
+from urllib import request, parse
 
 if __name__ == "__main__":
     email = {"email": sys.argv[2]}
-    email = urllib.parse.urlencode(email)
-    req = urllib.request.Request(sys.argv[1], email)
-    with urllib.request.urlopen(req) as res:
+    email = parse.urlencode(email)
+    req = request.Request(sys.argv[1], email)
+    with request.urlopen(req) as res:
         print(res.read().decode("utf-8"))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ sending a request to a server and handling the http or the url error """
 import sys
-import urllib.request, urllib.error
+from urllib import error, request
 
 if __name__ == "__main__":
     try:
-        req = urllib.request.Request(sys.argv[1])
-        with urllib.request.urlopen(req) as res:
+        req = request.Request(sys.argv[1])
+        with request.urlopen(req) as res:
             print(res.read().decode("utf-8"))
-    except urllib.error.HTTPError as e:
+    except error.HTTPError as e:
         print("Error code: {}".format(e.code))
