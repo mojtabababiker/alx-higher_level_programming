cef55193e900e2d60d3a40552d4926d72fe70546
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 09:03:13 -0800

    Edit task 8, project 0x11

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,15 +4,18 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    data = {"q": ""}
-    data["q"] = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {'q': ''}
+    data['q'] = sys.argv[1] if len(sys.argv) > 1 else ""
     url = "http://0.0.0.0:5000/search_user"
-    res = requests.post(url, data=data)
-    if res.status_code == 204:  # no content response
+    try:
+        res = requests.post(url, data=data)
+        res.raise_for_status()
+
+        json_format = res.json()
+        print(f"[{json_format['id']}] {json_format['name']}")
+
+    except requests.exceptions.HTTPError:
+        print("Not a valid JSON")
+
+    except KeyError:
         print("No result")
-    else:
-        try:
-            json_format = res.json()
-            print(f"[{json_format['id']}] {json_format['name']}")
-        except requests.exception.JSONDecodeError:
-            print("Not a valid JSON")
