79ed9ccf57f28e26518721269ce18cb7772eed95
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 18:05:35 +0200

    Edit task 0, add __name__==__main__ condition, project 0x11

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(str(content)))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
