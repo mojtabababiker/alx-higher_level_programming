92606ebe666be001d46200e9f4231cb32cf96152
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 18:01:02 +0200

    Edit task 1, add __name__==__main__ and change a deprecated urllib method, project 0x11

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ retrive a cerian header value from a request with urllib """
 
-import sys
-import urllib.request
+if __name__ == "__main__":
+    import sys
+    import urllib.request
 
-
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as res:
-    print(res.info().get("X-Request-Id", None))
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as res:
+        print(res.headers.get("X-Request-Id", None))
