6af95e667917b58a5c75bec86422c0c60983a001
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 09:35:00 -0800

    Edit task 10, project 0x11

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,13 +6,14 @@ import requests
 if __name__ == "__main__":
     url = "https://api.github.com/users/{}".format(sys.argv[1])
     headers = {"Accept": "application/vnd.github+json",
-               "X-GitHub-Api-Version": "2022-11-28"
+               "X-GitHub-Api-Version": "2022-11-28",
                }
 
-    data = {"access_token": sys.argv[2]}
     try:
-        res = requests.post(url, headers=headers, data=data,
-                            auth=(sys.argv[1], sys.argv[2]))
+        res = requests.get(url, headers=headers,
+                           auth=(sys.argv[1],
+                                 sys.argv[2]))
+
         print(res.json()["id"])
 
     except (requests.exceptions.HTTPError, KeyError):
