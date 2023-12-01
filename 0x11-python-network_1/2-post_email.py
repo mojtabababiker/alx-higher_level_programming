ca007bc9f37980a7bde864f9dcde24bc01d5eb26
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 09:40:32 -0800

    Edit task 2, change email data type to BYTE, project 0x11

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,6 +6,7 @@ from urllib import request, parse
 if __name__ == "__main__":
     email = {"email": sys.argv[2]}
     email = parse.urlencode(email)
+    email = email.encode("ascii")
     req = request.Request(sys.argv[1], email)
     with request.urlopen(req) as res:
         print(res.read().decode("utf-8"))
