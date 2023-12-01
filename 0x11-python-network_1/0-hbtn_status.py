477b41e80bef336e54b62e7454d5383388efbce0
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 18:00:10 +0200

    Edit task 0, add __name__==__main__ condition, project 0x11

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """fetch server using urllib package"""
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as res:
-    content = res.read()
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(str(content)))
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(str(content)))
