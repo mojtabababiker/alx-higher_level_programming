221c81cd025a611521fe8caff3bf4047e3a42d59
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 18:12:12 +0200

    Edit task 3, project 0x11

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ sending a request to a server and handling the http or the url error """
 import sys
-import urllib.request
-import urllib.error
+import urllib.request, urllib.error
 
-
-try:
-    req = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(req) as res:
-        print(str(res.read()))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    try:
+        req = urllib.request.Request(sys.argv[1])
+        with urllib.request.urlopen(req) as res:
+            print(res.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
