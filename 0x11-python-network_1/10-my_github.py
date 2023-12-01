b611cfb9db08c87c7407eba9cd8c354f918b400c
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 18:33:46 +0200

    Edit task 10, project 0x11

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,16 +3,17 @@
 import sys
 import requests
 
-url = "https://api.github.com/users/{}".format(sys.argv[1])
-headers = {"Accept": "application/vnd.github+json",
-           "X-GitHub-Api-Version": "2022-11-28"
-           }
+if __name__ == "__main__":
+    url = "https://api.github.com/users/{}".format(sys.argv[1])
+    headers = {"Accept": "application/vnd.github+json",
+               "X-GitHub-Api-Version": "2022-11-28"
+               }
 
-data = {"access_token": sys.argv[2]}
-try:
-    res = requests.post(url, headers=headers, data=data,
-                        auth=(sys.argv[1], sys.argv[2]))
-    print(res.json()["id"])
+    data = {"access_token": sys.argv[2]}
+    try:
+        res = requests.post(url, headers=headers, data=data,
+                            auth=(sys.argv[1], sys.argv[2]))
+        print(res.json()["id"])
 
-except (requests.exceptions.HTTPError, KeyError):
-    print("None")
+    except (requests.exceptions.HTTPError, KeyError):
+        print("None")
