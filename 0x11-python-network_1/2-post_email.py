9de55c6f3d4c7eb7e2f5bd238c9d62fae24d1969
mojtabababiker <mojmohammad98@gmail.com>
2023-12-01 18:09:23 +0200

    Edit task 2, project 0x11

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ send an email using urllib.request module """
 import sys
-import urllib.request
-import urllib.parse
+import urllib.request, urllib.parse
 
-email = {"email": sys.argv[2]}
-email = urllib.parse.urlencode(email)
-req = urllib.request.Request(sys.argv[1], email)
-with urllib.request.urlopen(req) as res:
-    print(str(res.read()))
+if __name__ == "__main__":
+    email = {"email": sys.argv[2]}
+    email = urllib.parse.urlencode(email)
+    req = urllib.request.Request(sys.argv[1], email)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
