63b8a588603b9efa2ec0e498f0749ad96d79b658
mojtabababiker <mojmohammad98@gmail.com>
2023-12-02 07:42:52 +0200

    Edit task 102, set the requered authuntications, project 0x10

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script inject message to be printed on the response body
-curl -X "PUT" --location-trusted -u "user_id = 98":98 -Ls "0.0.0.0:5000/catch_me"
+curl --location-trusted -d "user_id=98":98 -H "Origin: School" \
+     -X "PUT" -Ls "0.0.0.0:5000/catch_me"
